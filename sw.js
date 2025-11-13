836b98e4fa6d206747dbd77b8fe59ab63b7bd2de
antuabinash <150573436+antuabinash@users.noreply.github.com>
2025-11-13 23:44:47 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // UPDATE: Cache version bumped to force refresh
-const CACHE_NAME = 'student-data-cache-v7-spark';
+const CACHE_NAME = 'student-data-cache-v8-final';
 
 const urlsToCache = [
   './',
