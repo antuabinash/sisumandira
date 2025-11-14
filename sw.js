41e649d05fd5f4f79538fc6444528e81101e069a
antuabinash <150573436+antuabinash@users.noreply.github.com>
2025-11-14 14:57:55 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // UPDATE: Cache version bumped to force refresh
-const CACHE_NAME = 'student-data-cache-v8-final';
+const CACHE_NAME = 'student-data-cache-v9-final-keys';
 
 const urlsToCache = [
   './',
@@ -30,11 +30,9 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Cache hit - return response
         if (response) {
           return response;
         }
-        // Not in cache - fetch from network
         return fetch(event.request);
       }
     )
