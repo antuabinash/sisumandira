b708dc7550ce76e710df7396f75c7bcf7d58af5d
antuabinash <150573436+antuabinash@users.noreply.github.com>
2025-11-16 14:47:32 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // UPDATE: Cache version bumped to force refresh
-const CACHE_NAME = 'student-data-cache-v12-card-gen';
+const CACHE_NAME = 'student-data-cache-v13-odia-text';
 
 const urlsToCache = [
   './',
