afb5e9f9f307d06d56a4fa8073f1b80cc7f950bb
antuabinash <150573436+antuabinash@users.noreply.github.com>
2025-11-16 14:36:47 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // UPDATE: Cache version bumped to force refresh
-const CACHE_NAME = 'student-data-cache-v11-admin-phone-fix';
+const CACHE_NAME = 'student-data-cache-v12-card-gen';
 
 const urlsToCache = [
   './',
